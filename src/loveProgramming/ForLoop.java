0f45b0fbf1cf827191afb7e1067286a9cfdb6e52
https://github.com/Rajeshwari10Bligar/WhileLoop.git <rajeshwaribaligar@gmail.com>
2019-01-08 00:00:05 +0000

    ForLoop

---
+++ src/loveProgramming/ForLoop.java
@@ -0,0 +1,13 @@
+package loveProgramming;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int i=50;
+        for (i=50; i>=0; i--)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
